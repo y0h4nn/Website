e21c57d26918c61276e874e25b2b63e091a92244
Eijebong <eijebong@bananium.fr>
2015-06-11 03:25:27 +0200

    Start of modules.

---
+++ application/modules/auth/__init__.py
@@ -0,0 +1,4 @@
+from flask import Module
+
+module = Module(__name__, name='auth', static_path="/static/auth")
+
---
+++ application/modules/auth/views.py
@@ -0,0 +1,5 @@
+from . import module
+
+@module.route("/a")
+def lol():
+    return "coucou"
