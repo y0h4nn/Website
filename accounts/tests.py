5dc3c8b650a9284b81f30a99c03c240780f6774e
Arnaud Levaufre <arnaud@bananium.fr>
2015-07-24 16:29:50 +0200

    Some auth tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,50 @@
+import os
 from django.test import TestCase
+from django.contrib.auth.models import User
+from . import backends
 
-# Create your tests here.
+class NormalAithTest(TestCase):
+    def setUp(self):
+        self.auth_backend = backends.NormalAuth()
+        self.imap_backend = backends.ImapAuth()
+
+        # Be carefull when changing user password ad they are hardcoded
+        self.ext_user = User.objects.create_user('AAA', 'AAA@exemple.com', 'AAA')
+        self.enib_user = User.objects.create_user('BBB', 'BBB@enib.fr', 'BBB')
+
+    def test_auth_mail(self):
+        auth_user = self.auth_backend.authenticate(email=self.ext_user.email, password='AAA')
+        self.assertEqual(self.ext_user.username, auth_user.username)
+
+    def test_auth_enib(self):
+        # auth with username as it's the email adress without le @enib.fr
+        auth_user = self.auth_backend.authenticate(email=self.enib_user.username, password='BBB')
+        self.assertEqual(self.enib_user.username, auth_user.username)
+
+    def test_auth_reject(self):
+        auth_user = self.auth_backend.authenticate(email='INVALID', password='INVALID')
+        self.assertIsNone(auth_user)
+
+    def test_auth_ext_no_full_mail(self):
+        auth_user = self.auth_backend.authenticate(email=self.ext_user.username, password='CCC')
+        self.assertIsNone(auth_user)
+
+    def test_auth_wrong_passwd(self):
+        auth_user = self.auth_backend.authenticate(email=self.enib_user.email, password='INVALID')
+        self.assertIsNone(auth_user)
+
+    def test_imap_enib_fail(self):
+        auth_user = self.imap_backend.authenticate(email='INVALID@enib.fr', password='INVALID')
+        self.assertIsNone(auth_user)
+
+    def test_imap_enib_success(self):
+        try:
+            mail = os.environ['test_mail']
+            password = os.environ['test_password']
+        except:
+            mail = password = None
+        auth_user = self.imap_backend.authenticate(email=mail, password=password)
+        self.assertIsNotNone(auth_user)
+        self.assertEqual(auth_user.email, mail)
+
+
