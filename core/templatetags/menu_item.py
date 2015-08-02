e153e5f2e747d5895574290988e0352eac0a5717
Arnaud Levaufre <arnaud@bananium.fr>
2015-08-02 21:43:05 +0200

    Keep it simple. Reword the menu
    In fact this kind of simple menu will be sufficient as we will most
    certainly add only one entry to per app in the main menu which redirect
    to a given index of the app. The app then can do whatever it wants. I
    plan to create a submenu "wrapper" with tags to create menu item in an
    unified way so each app can have a custom submenu without bkreaking the
    design if they extends base.
    
    Of course login/logout will not stay in the current menu and will have a
    special treatment, so for the moment the dual selected menu is ok.

--- core/templatetags/menu_item.py
+++ core/templatetags/menu_item.py
@@ -4,11 +4,15 @@ from django.core.urlresolvers import reverse, resolve
 register = template.Library()
 
 @register.inclusion_tag('core/tags/menu_item.html', takes_context=True)
-def menu_item(context, name, view, *args, **kwargs):
+def base_menu_item(context, name, view, *args, **kwargs):
     request = context['request']
     selected = False
+    try:
+        namespace, _ = view.split(':')
+    except ValueError:
+        namespace = None
 
-    if resolve(request.path).view_name == view:
+    if namespace in resolve(request.path).namespaces:
         selected = True
 
     return {
