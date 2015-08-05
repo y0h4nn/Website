55b95cde64067b100a893cb998b5ba367f5eb78b
Arnaud Levaufre <arnaud@bananium.fr>
2015-08-05 19:33:56 +0200

    Menu for apps

--- core/templatetags/menu_item.py
+++ core/templatetags/menu_item.py
@@ -3,8 +3,8 @@ from django.core.urlresolvers import reverse, resolve
 
 register = template.Library()
 
-@register.inclusion_tag('core/tags/base_menu_item.html', takes_context=True)
-def base_menu_item(context, name, view, *args, **kwargs):
+@register.inclusion_tag('core/tags/menu_item.html', takes_context=True)
+def _base_menu_item(context, name, view, *args, **kwargs):
     request = context['request']
     selected = False
     try:
@@ -13,6 +13,20 @@ def base_menu_item(context, name, view, *args, **kwargs):
         namespace = None
 
     if namespace in resolve(request.path).namespaces:
+        selected = True
+
+    return {
+        'name': name,
+        'url': reverse(view, args=args, kwargs=kwargs),
+        'selected': 'selected' if selected else ''
+    }
+
+@register.inclusion_tag('core/tags/menu_item.html', takes_context=True)
+def menu_item(context, name, view, *args, **kwargs):
+    request = context['request']
+    selected = False
+
+    if view == resolve(request.path).view_name:
         selected = True
 
     return {
