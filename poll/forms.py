6bed3f8de7b9a36f2d85fd916f63968c6f825bf3
Eijebong <eijebong@bananium.fr>
2015-08-09 13:59:21 +0200

    Change del button color to red.

--- poll/forms.py
+++ poll/forms.py
@@ -10,10 +10,10 @@ class QuestionWidget(forms.widgets.TextInput):
         self.data = parent.data
 
     def render(self, name="", value="", **kwargs):
-        plus = "<button onclick=\"add_response({qid});return false;\">+</button><button onclick=\"del_question({qid});return false;\">x</button><div id=\"q_a{real_qid}\"></div>"
+        plus = "<button onclick=\"add_response({qid});return false;\">+</button><button class=\"red_button\" onclick=\"del_question({qid});return false;\">x</button><div id=\"q_a{real_qid}\"></div>"
         plus += "<div id=\"q_a{qid}\">"
         for i, answer in enumerate(self.answers):
-            plus += "<div id=\"div_q" + self.qid[1:] + "_" + str(i + 1) + "\"><label for=\""+ answer +"\">Réponse: </label>" + super().render(name=answer, value=self.data[answer], attrs={"id": answer}) + "<button onclick=\"del_answer(" + self.qid[1:] + "," + str(i + 1) + ");return false;\">x</button></div>"
+            plus += "<div id=\"div_q" + self.qid[1:] + "_" + str(i + 1) + "\"><label for=\""+ answer +"\">Réponse: </label>" + super().render(name=answer, value=self.data[answer], attrs={"id": answer}) + "<button class=\"red_button\" onclick=\"del_answer(" + self.qid[1:] + "," + str(i + 1) + ");return false;\">x</button></div>"
         plus += "</div></fieldset>"
         return "<fieldset id=\"div_q" + self.qid[1:] + "\"><label for=\"" + self.qid + "\">Question: </label>" + super().render(name=name, value=value, **kwargs) + plus.format(qid=self.qid[1:], real_qid=self.qid)
 
