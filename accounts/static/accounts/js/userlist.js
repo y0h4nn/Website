9f9ab64d3f37a3159c3b5df74163fd54416e68d9
Arnaud Levaufre <arnaud@bananium.fr>
2015-08-10 21:44:35 +0200

    basic user search

--- accounts/static/accounts/js/userlist.js
+++ accounts/static/accounts/js/userlist.js
@@ -29,11 +29,14 @@ function queryJson(url, data, cb){
     var UserList = function(containerId){
         this.element = document.getElementById(containerId);
         this.searchInput = document.createElement('input');
+        this.listelement = document.createElement('ul');
+        this.listelement.setAttribute('class', 'userlist');
+        this.allUsers = []
+        this.matchingUsers = []
 
         this.populateUsers = function(json){   
             this.users = json['users'];
             for(var user of this.users){
-                user['element'] = document.createElement('li');
                 var link = document.createElement('a');
                     link.setAttribute('href', user['profile_url']);
                 var img = document.createElement('img');
@@ -44,28 +47,42 @@ function queryJson(url, data, cb){
                 var nameContainer = document.createElement('div');
                     nameContainer.innerHTML = user['display_name'];
 
+                user['element'] = document.createElement('li');
                 user['element'].appendChild(link);
                 link.appendChild(pictureContainer);
                 link.appendChild(nameContainer);
                 pictureContainer.appendChild(img);
             }
 
+            this.matchingUsers = this.users;
+            this.reset();
             this.render();
         };
 
-        this.render = function(){
-            var ul = document.createElement('ul');
-            ul.setAttribute('class', 'userlist');
+        this.match = function(user){
+            return new RegExp(this.searchInput.value).test(user['display_name']);
+        }
 
-            for(var user of this.users){
-                ul.appendChild(user['element']);
-            }
-
+        this.reset = function(){
             this.element.innerHTML = "";
-            this.element.appendChild(ul);
+            this.element.appendChild(this.searchInput);
+            this.element.appendChild(this.listelement);
+        };
+
+        this.render = function(){
+            this.listelement.innerHTML = "";
+            for(var user of this.matchingUsers){
+                this.listelement.appendChild(user['element']);
+            }
         };
 
         queryJson('', {}, this.populateUsers.bind(this));
+
+        this.searchInput.addEventListener('keyup', function(){
+            console.log(this.searchInput.value);
+            this.matchingUsers = this.users.filter(this.match, this);
+            this.render();
+        }.bind(this));
     }
 
     window.UserList = UserList;
