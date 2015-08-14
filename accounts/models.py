63e6c0ba5cd5d2f1210a55217c259bdd5a63548f
Arnaud Levaufre <arnaud@bananium.fr>
2015-08-14 22:05:27 +0200

    Fix error when creating user

--- accounts/models.py
+++ accounts/models.py
@@ -6,6 +6,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from django.conf import settings
+from webmail.models import WebmailSettings
 
 
 SEMESTERS = [(None, 'Aucun'),]
