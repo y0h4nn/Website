89ec9038c4fe9901bdf7c9c4dcbab70051c0a785
Arnaud Levaufre <arnaud@bananium.fr>
2015-08-15 20:59:40 +0200

    Retractable menu

---
+++ static/js/user_container.js
@@ -0,0 +1,54 @@
+
+window.addEventListener('load', function(){
+  var button = document.getElementById('toggle_user_container');
+  var explicitClose = false;
+  var explicitOpen = false
+  var previousSize = document.body.clientWidth;
+
+  function retract(){
+      button.parentNode.setAttribute('class', 'retracted');
+      button.firstChild.setAttribute('class', 'fa fa-chevron-left');
+      localStorage.setItem('userContainerRetracted', true);
+  }
+  function expand(){
+      button.parentNode.setAttribute('class', '');
+      button.firstChild.setAttribute('class', 'fa fa-chevron-right');
+      localStorage.setItem('userContainerRetracted', false);
+  }
+
+  button.addEventListener('click', function(event){
+    if(button.parentNode.getAttribute('class') == 'retracted'){
+      expand();
+      explicitOpen = true;
+    }
+    else{
+      retract();
+      explicitClose = true;
+    }
+  });
+
+  window.addEventListener('resize', function(event){
+    if(document.body.clientWidth < 1280){
+      if(document.body.clientWidth < previousSize){
+        retract();
+      }
+    }
+    else if (!explicitClose){
+      expand();
+    }
+    previousSize = document.body.clientWidth;
+  });
+
+
+  if(localStorage.getItem('userContainerRetracted') === "true"){
+    retract();
+    explicitClose = true;
+  }
+
+  if(document.body.clientWidth < 1280){
+    retract()
+  }
+  else if (!explicitClose){
+    expand()
+  }
+});
