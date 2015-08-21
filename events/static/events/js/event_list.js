d08a154329d0c55de22a2a9b818ba857d8db22c1
Eijebong <eijebong@bananium.fr>
2015-08-21 11:18:13 +0200

    Fixes #15

--- events/static/events/js/event_list.js
+++ events/static/events/js/event_list.js
@@ -1,14 +1,18 @@
 'use strict';
 
 function update_button(data){
+	var cur_nb = parseInt(this.parentNode.getElementsByClassName("inscription_count")[0].innerHTML);
 	if(data['registered'] === 1){
 		this.innerHTML = "Se désinscrire";
 		this.className = "red_button";
+		cur_nb += 1;
 	}
 	else{
 		this.className = "";
 		this.innerHTML = "S'inscrire";
+		cur_nb -= 1;
 	}
+	this.parentNode.getElementsByClassName("inscription_count")[0].innerHTML = cur_nb;
 }
 
 function inscription(elmt, eid){
