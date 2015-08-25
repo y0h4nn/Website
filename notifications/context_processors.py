fe14b8770074adfe8e71df27e554c6ae0e3d2583
Arnaud Levaufre <arnaud@bananium.fr>
2015-08-25 23:49:57 +0200

    Fix context processor

--- notifications/context_processors.py
+++ notifications/context_processors.py
@@ -1,4 +1,9 @@
 from . import models
 
 def check_notifications(request):
-    return {'user_have_notifications': models.Notification.has_notification(request.user) }
+    if request.user.is_active:
+        has = models.Notification.has_notification(request.user)
+    else:
+        has = False
+
+    return {'user_have_notifications':  has}
