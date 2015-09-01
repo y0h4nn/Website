7d1af72d7b78a6faa525fb9541f629be6f70e5a3
Arnaud Levaufre <arnaud@bananium.fr>
2015-09-02 01:56:36 +0200

    Add migration to delete old history entries which are not compatible with the new history system

---
+++ shop/migrations/0007_auto_20150902_0153.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+def delete_history(apps, schema_editor):
+    BuyingHistory = apps.get_model("shop", "BuyingHistory")
+    BuyingHistory.objects.all().delete()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('shop', '0006_auto_20150902_0104'),
+    ]
+
+    operations = [
+        migrations.RunPython(delete_history)
+    ]
