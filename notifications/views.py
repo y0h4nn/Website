49dc5dc58b88fd2953e9a3bc79b96ff05ca6efc0
Eijebong <eijebong@bananium.fr>
2015-09-04 07:18:47 +0200

    Fixes #44

--- notifications/views.py
+++ notifications/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from . import models
 
 
+@login_required()
 def index(request):
     context = {
         'notifications': models.Notification.objects.filter(user=request.user,read=False).order_by('date').all().reverse()
@@ -10,6 +12,7 @@ def index(request):
 
     return render(request, 'notifications/index.html', context)
 
+@login_required()
 def read(request, nid):
     notification = get_object_or_404(models.Notification, id=nid, user=request.user)
     notification.read = True
