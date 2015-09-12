dc4f01b9b96bc852bb1f413a11772f0468977bba
Eijebong <eijebong@bananium.fr>
2015-09-13 00:21:36 +0200

    Add a template tag to pretty print phone numbers.

---
+++ core/templatetags/phone.py
@@ -0,0 +1,9 @@
+from django import template
+register = template.Library()
+
+@register.filter
+def phone(number):
+    print(number)
+    print(tuple(number))
+    return ("{} " * 5).format(*[number[i:i+2] for i in range(0, len(number), 2)])
+
