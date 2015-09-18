56fe97d8e752aeb11ba33dbd2aa7be22fcacd4f3
Eijebong <eijebong@bananium.fr>
2015-09-18 23:59:44 +0200

    Add a migration to delete old commands/inscriptions since the date would be messed up.

---
+++ pizza/migrations/0004_auto_20150918_2357.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+def migrate(apps, schema_editor):
+    with schema_editor.connection.cursor() as cursor:
+        cursor.execute("DELETE FROM pizza_command")
+        cursor.execute("DELETE FROM pizza_inscription")
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('pizza', '0003_auto_20150918_2233'),
+    ]
+
+    operations = [
+        migrations.RunPython(migrate),
+    ]
