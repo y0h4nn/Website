9cd555b9e5fa669f639b1a07b26cbb5ee7ded1cc
Eijebong <eijebong@bananium.fr>
2015-09-20 00:09:17 +0200

    Use mysql as default storage engine since

--- site_des_eleves/settings.py
+++ site_des_eleves/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'site_des_eleves.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sde',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
