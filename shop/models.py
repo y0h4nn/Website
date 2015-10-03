16700c2c8e22b6ccccff9d1fa6d11ebbe70b5b4d
Eijebong <eijebong@bananium.fr>
2015-10-03 17:39:32 +0200

    Fix models.

--- shop/models.py
+++ shop/models.py
@@ -187,8 +187,8 @@ TYPES = [
 class BuyingHistory(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
     type = models.CharField(max_length=10, choices=TYPES)
-    product = models.ForeignKey(Product ,null=True, default=None)
-    pack = models.ForeignKey(Packs, null=True, default=None)
+    product = models.ForeignKey(Product ,null=True)
+    pack = models.ForeignKey(Packs, null=True)
     date = models.DateTimeField(auto_now_add=True)
     payment_mean = models.CharField(max_length=10, choices=MEANS_OF_PAYMENT)
 
