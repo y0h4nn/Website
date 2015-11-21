207fe6d4bef36e581bb2f720e0bacb209dad147a
Eijebong <eijebong@bananium.fr>
2015-11-21 21:16:41 +0100

    Remove the useless APIs URLs too...

--- enibar/urls.py
+++ enibar/urls.py
@@ -3,10 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^note$', views.request_note, name="request_note"),
-    url(r'^category$', views.request_category, name="request_category"),
-    url(r'^price_description$', views.request_price_description, name="request_price_description"),
-    url(r'^product$', views.request_product, name="request_product"),
-    url(r'^price$', views.request_price, name="request_price"),
     url(r'^history$', views.request_history, name="request_history"),
 ]
 
