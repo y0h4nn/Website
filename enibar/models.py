d7dec129319ca7d643dbdb1cfa2cba7aa68575eb
Eijebong <eijebong@bananium.fr>
2015-12-07 21:46:12 +0100

    Add date, quantity and percentage to HistoryLine.

--- enibar/models.py
+++ enibar/models.py
@@ -10,10 +10,13 @@ class Note(models.Model):
 
 class HistoryLine(models.Model):
     foreign_id = models.IntegerField()
+    date = models.DateTimeField(default=None)
     note = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
     product = models.CharField(max_length=255)
     price_name = models.CharField(max_length=255)
-    quantity = models.IntegerField()
+    quantity = models.IntegerField(default=0)
+    liquid_quantity = models.IntegerField(default=0)
+    percentage=models.DecimalField(max_digits=10, decimal_places=2, default=0)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
