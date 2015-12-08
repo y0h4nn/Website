c9c118039a0d6ed59cbc474feb5df8d889e39077
Arnaud Levaufre <arnaud@bananium.fr>
2015-12-08 18:02:35 +0100

    Fix migration for date.
    A default to None with no null=True is not possible. And it seems a
    null=True and a blank=False is not possible for already existing
    entries. So django failed it's system check

--- enibar/models.py
+++ enibar/models.py
@@ -10,7 +10,7 @@ class Note(models.Model):
 
 class HistoryLine(models.Model):
     foreign_id = models.IntegerField()
-    date = models.DateTimeField(default=None)
+    date = models.DateTimeField(default=None, null=True, blank=True)
     note = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
     product = models.CharField(max_length=255)
