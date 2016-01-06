aa0269e22e796ca2abdeae972c4a843f209c71c5
Eijebong <eijebong@bananium.fr>
2016-01-06 11:05:21 +0100

    Fixes #93

--- static/js/formset.js
+++ static/js/formset.js
@@ -5,7 +5,7 @@ var FormSet = function(empty_id, button_id, formset_id, nb_forms_id){
     this.nb_forms = document.getElementById(nb_forms_id);
     this.button.addEventListener('click', function(event){
         event.preventDefault();
-        var nb = this.formset.getElementsByTagName('div').length
+        var nb = this.formset.getElementsByTagName('tr').length
         var node = document.createElement('tr');
         node.innerHTML = this.empty.innerHTML.replace(new RegExp('__prefix__', 'g'), nb)
         this.nb_forms.value = nb;
