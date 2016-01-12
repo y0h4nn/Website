28a8486170e10ae36268e2eec6e23099d0edab4f
Eijebong <eijebong@bananium.fr>
2016-01-12 13:35:26 +0100

    Add index on foreign_id in enibar models.

--- enibar/models.py
+++ enibar/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 
 class Note(models.Model):
-    foreign_id = models.IntegerField(unique=True)
+    foreign_id = models.IntegerField(unique=True, db_index=True)
     nickname = models.CharField(max_length=255)
     mail = models.EmailField()
     note = models.DecimalField(max_digits=10, decimal_places=2)
 
 
 class HistoryLine(models.Model):
-    foreign_id = models.IntegerField()
+    foreign_id = models.IntegerField(db_index=True)
     date = models.DateTimeField(default=None, null=True, blank=True)
     note = models.CharField(max_length=255)
     category = models.CharField(max_length=255)
