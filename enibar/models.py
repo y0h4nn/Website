141bf77eb3b033bd42fde43fd17dcb22c434fde9
Eijebong <eijebong@bananium.fr>
2016-01-13 01:44:46 +0100

    Perfs *10 on show_history by adding an index on nickname.

--- enibar/models.py
+++ enibar/models.py
@@ -11,7 +11,7 @@ class Note(models.Model):
 class HistoryLine(models.Model):
     foreign_id = models.IntegerField(db_index=True)
     date = models.DateTimeField(default=None, null=True, blank=True)
-    note = models.CharField(max_length=255)
+    note = models.CharField(max_length=255, db_index=True)
     category = models.CharField(max_length=255)
     product = models.CharField(max_length=255)
     price_name = models.CharField(max_length=255)
