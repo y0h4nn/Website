8b10af95eac11553ab68ea1766eda9c55391931f
Eijebong <eijebong@bananium.fr>
2016-03-15 15:20:33 +0100

    Revert "Fix polls..."
    This reverts commit d649fab22732beedfbbc48b210925c75398b9cad.

--- poll/models.py
+++ poll/models.py
@@ -23,10 +23,7 @@ class Poll(models.Model):
         return timezone.now() >= self.end_date
 
     def can_vote(self, user):
-        auth = user.is_authenticated()
-        contrib = not self.contributor_only and is_contributor(user)
-        group = self.group in user.groups.all()
-        return auth and contrib and group
+        return user.is_authenticated() and (not self.contributor_only or is_contributor(user)) and self.group in user.groups.all()
 
     def can_see_results(self, user):
         return user.is_authenticated and self.group in user.groups.all()
@@ -52,5 +49,3 @@ class Voter(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='voted_questions')
     poll = models.ForeignKey(Poll)
 
-    def __repr__(self):
-        return "{} - {}".format(self.user.username, self.poll.title)
